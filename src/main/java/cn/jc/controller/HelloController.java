e4be258bb9bd1cda9e2f26284faf3efaf40f31e7
user007 <22589675962@qq.com>
2019-10-17 01:25:55 +0800

    Initial commit

---
+++ src/main/java/cn/jc/controller/HelloController.java
@@ -0,0 +1,11 @@
+package cn.jc.controller;
+
+/**
+ * @author equation
+ * @date 2019/10/17
+ */
+public class HelloController {
+    public static void main(String[] args) {
+        System.out.println("Test Git");
+    }
+}
